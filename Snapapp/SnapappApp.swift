a1fb332311feb4dda80c6fb9b40cc65ddf54b0fd
w <legion25@gma>
2025-02-19 17:27:30 -0500

    Initial Commit

---
+++ Snapapp/SnapappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapappApp.swift
+//  Snapapp
+//
+//  Created by Ernesto  Lopez on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapappTests/SnapappTests.swift
@@ -0,0 +1,16 @@
+//
+//  SnapappTests.swift
+//  SnapappTests
+//
+//  Created by Ernesto  Lopez on 2/19/25.
+//
+
+import Testing
+
+struct SnapappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
